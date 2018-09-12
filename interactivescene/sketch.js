ac6f708f4af705c01a2321e9efe6700e2bd205da
TyndallJ <43014121+TyndallJ@users.noreply.github.com>
2018-09-12 14:08:19 -0600

    uh

--- interactivescene/sketch.js
+++ interactivescene/sketch.js
@@ -6,9 +6,19 @@
 // - describe what you did to take this project "above and beyond"
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+    createCanvas(400, 400);
+    background(255);
 }
 
 function draw() {
+  fill(255,0,0)
+  if (mouseIsPressed && keyCode === 69)
+    ellipse(mouseX, mouseY, 30, 30)
+  if (mouseIsPressed && keyCode === 82)
+    rect(mouseX, mouseY, 30, 30)
+  if (keyCode === 87)
+	background(255)
+  if (keyCode === 66)
+    background(0)
 
 }

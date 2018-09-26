555616fa24356bd8cb42b8df46ac894c088570e2
TyndallJ <43014121+TyndallJ@users.noreply.github.com>
2018-09-26 13:54:52 -0600

    oiuytr

--- p5-1/sketch.js
+++ p5-1/sketch.js
@@ -1,14 +1,82 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Trafficlight Demo
+// Tyndall J
+// September 25, 2018
+
+// GOAL: Make a 'traffic light' simulator.
+
+
+let state;
+let lastTimeSwitchedColor;
+
+const RED_LIGHT_DURATION = 3000;
+const GREEN_LIGHT_DURATION = 4000;
+const YELLOW_LIGHT_DURATION = 1000;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
+  state = 1;
+  lastTimeSwitchedColor = 0;
 }
 
 function draw() {
+  background(255);
+  drawOutlineOfLights();
+  checkForStateChange();
+  displayCorrectLight();
+}
 
+function checkForStateChange() {
+  let elapsedTime = millis() - lastTimeSwitchedColor;
+  if (state === 1 && elapsedTime >= RED_LIGHT_DURATION) {
+    state = 2;
+    lastTimeSwitchedColor = millis();
+  }
+  else if (state === 2 && elapsedTime >= GREEN_LIGHT_DURATION) {
+    state = 3;
+    lastTimeSwitchedColor = millis();
+  }
+  else if (state === 3 && elapsedTime >= YELLOW_LIGHT_DURATION) {
+    state = 1;
+    lastTimeSwitchedColor = millis();
+  }
+}
+
+function displayCorrectLight() {
+  if (state === 1) {
+    displayRedLight();
+  }
+  else if (state === 2) {
+    displayGreenLight();
+  }
+  else if (state === 3) {
+    displayYellowLight();
+  }
+}
+
+function displayRedLight() {
+  fill(255, 0, 0);
+  ellipse(width/2, height/2 - 65, 50, 50); //top
+}
+
+function displayYellowLight() {
+  fill(255, 255, 0);
+  ellipse(width/2, height/2, 50, 50); //middle
+}
+
+function displayGreenLight() {
+  fill(0, 255, 0);
+  ellipse(width/2, height/2 + 65, 50, 50); //bottom
+}
+
+function drawOutlineOfLights() {
+  //box
+  rectMode(CENTER);
+  fill(0);
+  rect(width/2, height/2, 75, 200, 10);
+
+  //lights
+  fill(255);
+  ellipse(width/2, height/2 - 65, 50, 50); //top
+  ellipse(width/2, height/2, 50, 50); //middle
+  ellipse(width/2, height/2 + 65, 50, 50); //bottom
 }

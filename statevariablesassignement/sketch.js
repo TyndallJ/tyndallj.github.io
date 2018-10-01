a91d9bfc569e5722e62e082dd5ab34ea4aae08b4
TyndallJ <43014121+TyndallJ@users.noreply.github.com>
2018-10-01 13:15:29 -0600

    sdfdfdf

---
+++ statevariablesassignement/sketch.js
@@ -0,0 +1,24 @@
+// Mario Kart 64 Character Select
+// Tyndall Johnston
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+//Variables
+let img;
+let x,y;
+
+function preload() {
+  img = loadImage("assets/selectcharacter.png");
+}
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  image(img, 0, 0, windowWidth, windowHeight);
+}
+
+function draw() {
+
+}

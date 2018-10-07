bfb53e4dee92f8eee7f41065caefb68f961f5a0f
TyndallJ <43014121+TyndallJ@users.noreply.github.com>
2018-10-07 11:43:57 -0600

    Update statevariablesassignement/sketch.js

--- statevariablesassignement/sketch.js
+++ statevariablesassignement/sketch.js
@@ -19,7 +19,6 @@ function preload() {
 function setup() {
   createCanvas(backgroundWidth, backgroundHeight);
   image(img, 0, 0, backgroundWidth, backgroundHeight);
-  rect(119,315,322,380);
 }
 
 function draw() {
@@ -29,8 +28,37 @@ function draw() {
 
 
 function findCharacter(){
-  if (mouseX < 322 && mouseY < 380){
-    background(220);
+  //Marios Location
+  if (mouseX < width*0.275 && mouseX > width*0.07 && mouseY < height*0.58 && mouseY > height*0.255){
+    rect(119,315,322,380);
+  }
+  //Luigis Location
+  else if (mouseX < width*0.492 && mouseX > width*0.29 && mouseY < height*0.58 && mouseY > height*0.255){
+    rect(466,315,322,380);
+  }
+  //Peachs Location
+  else if (mouseX < width*0.709 && mouseX > width*0.51 && mouseY < height*0.58 && mouseY > height*0.255){
+    rect(810,315,322,380);
+  }
+  //Toads Location
+  else if (mouseX < width*0.926 && mouseX > width*0.73 && mouseY < height*0.58 && mouseY > height*0.255){
+    rect(1154,315,322,380);
+  }
+  //Yoshis Location
+  else if (mouseX < width*0.275 && mouseX > width*0.07 && mouseY < height*0.93 && mouseY > height*0.60){
+    rect(119,726,322,380);
+  }
+  //Donkey Kongs Location
+  else if (mouseX < width*0.492 && mouseX > width*0.29 && mouseY < height*0.93 && mouseY > height*0.60){
+    rect(466,726,322,380);
+  }
+  //Warios Location
+  else if (mouseX < width*0.709 && mouseX > width*0.51 && mouseY < height*0.93 && mouseY > height*0.60){
+    rect(810,726,322,380);
+  }
+  //Bowsers Location
+  else if (mouseX < width*0.926 && mouseX > width*0.73 && mouseY < height*0.93 && mouseY > height*0.60){
+    rect(1154,726,322,380);
   }
   else{
     image(img, 0, 0, backgroundWidth, backgroundHeight);
